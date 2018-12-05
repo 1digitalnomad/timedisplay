a3944331907d4cae983470f5d93b5c40158d1558
Raul <Takethefirststep@Rauls-iMac.attlocal.net>
2018-12-04 19:25:10 -0800

    display time app

---
+++ apps/time_display/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+from time import gmtime, strftime
+
+# Create your views here.
+def index(request):
+    context = {
+        'time': strftime("%m-%d-%Y %H:%M %p", gmtime())
+    }
+    return render(request, 'time_display/index.html', context) 
